a8bd446816a98849dd8fa5b255f50c0c7799f1be
Codephics <152254639+codephics@users.noreply.github.com>
2024-07-10 17:07:37 +0600

    # Database
    # Database

--- database/migrations/backups/ecomxpress.sql
+++ database/migrations/backups/ecomxpress.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 10, 2024 at 12:55 PM
+-- Generation Time: Jul 10, 2024 at 01:07 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
