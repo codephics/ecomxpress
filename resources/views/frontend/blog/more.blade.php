ed6af747c119ba5f7525a41cefb792750d4bcfb9
Codephics <152254639+codephics@users.noreply.github.com>
2024-08-15 14:52:24 +0600

    Blog Route Improved
    Blog Route Improved

--- resources/views/frontend/blog/more.blade.php
+++ resources/views/frontend/blog/more.blade.php
@@ -7,7 +7,7 @@
 					<nav aria-label="breadcrumb">
 						<ol class="breadcrumb">
 							<li class="breadcrumb-item"><a href="{{ route('front.home') }}">Home</a></li>
-							<li class="breadcrumb-item active" aria-current="page">More Blog</li>
+							<li class="breadcrumb-item active" aria-current="page">More Blogs</li>
 						</ol>
 					</nav>
 				</div>
